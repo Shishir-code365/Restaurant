20d3d751c524cc970f41b4edc6b7e1b9efc29863
Shishir-code365 <dhakal.shishir2059@gmail.com>
2024-06-26 10:27:09 +0545

    cors update

--- index.ts
+++ index.ts
@@ -1,19 +1,12 @@
-import express from "express";
+// import express from "express";
+import express, { Request, Response, NextFunction } from 'express';
 import http from "http";
 import connectDb from "./utils/connection";
 import mainRouter from "./routes/index.routes";
 import { errorHandlerMiddleware } from "./middleware/errorHandler";
 import cors from "cors";
+
 const app = express();
-app.use(express.json({ limit: "32kb" }));
-
-app.use(express.urlencoded({ limit: "32kb", extended: true }));
-
-// app.use("/api/users", authRouter);
-app.use("/api", mainRouter);
-
-app.use(errorHandlerMiddleware);
-
 
 const corsOptions = {
   origin: [
@@ -22,27 +15,45 @@ const corsOptions = {
     "https://codynn.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-
     "http://localhost:5050",
     "http://127.0.0.1:5050",
-
     "http://localhost:5174",
     "http://localhost:5413",
-
     "https://betacompiler.codynn.com",
     "http://localhost:9787",
     "https://beta.codynn.com",
   ],
   credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
+app.use(express.json({ limit: "32kb" }));
+app.use(express.urlencoded({ limit: "32kb", extended: true }));
+app.use("/api", mainRouter);
+app.use(errorHandlerMiddleware);
+app.options("*", cors(corsOptions)); 
 
-const PORT = (process.env.PORT as string) || 4000;
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('Request Headers:', req.headers);
+
+  const originalSend = res.send;
+  res.send = (body: any) => {
+    console.log('Response Headers:', res.getHeaders());
+    return originalSend.call(res, body);
+  };
+
+  next();
+});
+
+
+const PORT = 3000;
 const server = http.createServer(app);
 
 connectDb()
   .then(() => {
-    server.listen(PORT, () => console.log("server is listening to port 4000"));
+    server.listen(PORT, () => console.log(`server is listening to port ${PORT}`));
   })
   .catch((err) => console.log("error", err));
