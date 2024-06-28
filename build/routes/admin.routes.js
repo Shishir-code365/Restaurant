189bbd3a784ab3acb9764eeb3c91ea16939e28af
Codynn <codynn23.gmail.com>
2024-06-28 16:27:40 +0800

    added build to .gitignore

--- build/routes/admin.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const admin_controller_1 = require("../controllers/admin.controller");
-const router = (0, express_1.Router)();
-router.route("/login").post(admin_controller_1.login);
-router.route("/register").post(admin_controller_1.register);
-exports.default = router;
--- build/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const router = (0, express_1.Router)();
-router.route("/login").post(user_controller_1.login);
-router.route("/register").post(user_controller_1.register);
-exports.default = router;
